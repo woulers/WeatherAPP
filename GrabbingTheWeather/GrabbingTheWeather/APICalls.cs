cf255dc86e38c6d66c6651c5706d5494a8c2fffe
Ryan <2015ryansamuelson@gmail.com>
2019-03-26 21:10:24 -0500

    First Inital Commit

---
+++ GrabbingTheWeather/GrabbingTheWeather/APICalls.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace GrabbingTheWeather
+{
+    public class APIResponce
+    {
+        public main main {get;set;}
+        public string name { get; set; }
+    }
+
+    public class main
+    {
+        public double temp { get; set; }
+    }
+
+    public class OpenWeatherMap
+    {
+        //pull key from app.config
+        //public static string OpenWeatherKey = "3e6309d288961c6cfb49323f8515da06";
+        public static string OpenWeatherKey = ConfigurationManager.AppSettings["OpenWeatherMapKey"];
+
+        public static string GetWeather(string Location)
+        {
+            //construct the API call
+            string Url = string.Format("http://api.openweathermap.org/data/2.5/weather?units=imperial&APPID={0}&q={1}", OpenWeatherKey, Location);
+
+            HttpClient client = new HttpClient();
+            string message = null;
+            HttpResponseMessage Response;
+
+            try
+            {
+                //GET request
+                Response = client.GetAsync(Url).Result;
+
+                //check to see if the call successful
+                if (Response.IsSuccessStatusCode)
+                {
+                    //use JSON to parse the message
+                    string tempMessage = Response.Content.ReadAsStringAsync().Result;
+
+                    //Fill temp with Json
+                    APIResponce temp = JsonConvert.DeserializeObject<APIResponce>(tempMessage);
+                    message = temp.name + " weather: " + temp.main.temp.ToString() + " degrees Fahrenheit";
+                }
+                else
+                {
+                    //location was not found
+                    message = "Invalid location";
+                }
+            
+                return message;
+                }
+
+            catch(Exception ex1)
+            {
+                Exception ex2 = new Exception("An error in the API call occured:" + ex1.Message);
+                throw ex2;
+            }
+            
+            
+            
+        } 
+    }
+}
---
+++ GrabbingTheWeather/GrabbingTheWeather/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrabbingTheWeather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            weather(textBox1.Text);
+        }
+
+        private void weather(string Location)
+        {
+            try
+            {
+                label2.Text = "loading...";
+                label2.Update();
+                label2.Text = OpenWeatherMap.GetWeather(Location);
+            }
+            catch (Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+            label2.Visible = true;
+            label2.Update();
+        }
+    }
+}
